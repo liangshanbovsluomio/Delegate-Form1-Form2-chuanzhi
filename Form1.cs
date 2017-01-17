ff57a5e240669255d47971e108e80c6872c0255a
liangshanbovsluomio <114848718@qq.com>
2017-01-17 12:15:01 +0800

    两窗口用委托传值

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 利用委托在两个窗口传值
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2(ShowStr);
+            f.Show();
+        }
+        void ShowStr(string str)
+        {
+            label1.Text = str;
+        }
+    }
+}
